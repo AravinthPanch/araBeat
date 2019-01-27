917ac8d4076280d30b3ff4727bc0e59157a961a2
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2019-01-28 00:59:49 +0100

    Add interrupt driven ECG readout

--- firmware/ecg/lib/max30003/max30003.cpp
+++ firmware/ecg/lib/max30003/max30003.cpp
@@ -81,8 +81,8 @@ void max30003::max30003_init()
     CNFG_GEN_r.bits.en_rbias = 1;  // Enable resistive bias
     CNFG_GEN_r.bits.imag = 2;      // Current magnitude = 10nA
     CNFG_GEN_r.bits.en_dcloff = 1; // Enable DC lead-off detection
-    // CNFG_GEN_r.bits.vth = 3;    // DC Lead-Off Voltage Threshold Selection VMID ± 500mV
-    // VMID ± 500mV
+    // CNFG_GEN_r.bits.vth = 3;    // DC Lead-Off Voltage Threshold Selection
+    // VMID ± 500mV VMID ± 500mV
     max30003_write_register(max30003::CNFG_GEN, CNFG_GEN_r.all);
     // max30003_write_register(max30003::CNFG_GEN, 0x081007);
     delay(100);
@@ -139,6 +139,25 @@ void max30003::max30003_init()
     // Serial.println("CNFG_RTOR_r");
     // Serial.println(CNFG_RTOR_r.all, BIN);
     // Serial.println(CNFG_RTOR_r.all, HEX);
+
+    // Enable interrupts register setting
+    max30003::EnableInterrupts_u EN_INT_r;
+    EN_INT_r.all = 0;
+    EN_INT_r.bits.en_eint = 1;   // Enable EINT interrupt
+    EN_INT_r.bits.en_rrint = 0;  // Disable R-to-R interrupt
+    EN_INT_r.bits.intb_type = 1; // Open-drain NMOS with internal pullup
+    max30003_write_register(max30003::EN_INT, EN_INT_r.all);
+
+    // Manage interrupts register setting
+    max30003::ManageInterrupts_u MNG_INT_r;
+    MNG_INT_r.bits.efit = 0b00011;   // Assert EINT w/ 4 unread samples
+    MNG_INT_r.bits.clr_rrint = 0b01; // Clear R-to-R on RTOR reg. read back
+    max30003_write_register(max30003::MNGR_INT, MNG_INT_r.all);
+
+    // Dyanmic modes config
+    max30003::ManageDynamicModes_u MNG_DYN_r;
+    MNG_DYN_r.bits.fast = 0; // Fast recovery mode disabled
+    max30003_write_register(max30003::MNGR_DYN, MNG_DYN_r.all);
 
     // Synchronize to begin new ecg recording
     max30003_write_register(max30003::SYNCH, 0x000000);
--- firmware/ecg/src/main.cpp
+++ firmware/ecg/src/main.cpp
@@ -17,13 +17,32 @@ max30003 ecg;
 
 const float RTOR_LSB_RES = 0.0078125f;
 
+// D2 on Arduino Nano
+const byte int1_pin = 2;
+// D3 on Arduino Nano
+const byte int2_pin = 3;
+
 /*******************************************************************************
  * Functions
  ********************************************************************************/
+// interrupt service routine
+volatile bool ecg_int_flag = 0;
+void ecg_callback()
+{
+    ecg_int_flag = 1;
+}
+
 void setup()
 {
     // setup serial port for plotting
     Serial.begin(115200);
+
+    // setup status LED
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    // setup interrupt
+    pinMode(int1_pin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(int1_pin), ecg_callback, CHANGE);
 
     // setup chipselect pin
     pinMode(MAX30003_CS_PIN, OUTPUT);
@@ -38,10 +57,6 @@ void setup()
 
     // initialize max30003 chip
     ecg.max30003_init();
-
-    // while (1)
-    // {
-    // };
 }
 
 // send data to Protocental GUI
@@ -86,33 +101,83 @@ void send_data_to_pde_plot(int16_t ecg_sample, uint16_t r_to_r, uint16_t bpm)
  * R-R is always above 0 at 20V/V ECG Gain in the serial plotter, when no ac
  * noise is present ac noise in the system comes from laptop charger, monitor HDMI
  ********************************************************************************/
+const int EINT_STATUS_MASK = 1 << 23;
+const int FIFO_OVF_MASK = 0x7;
+const int FIFO_VALID_SAMPLE_MASK = 0x0;
+const int FIFO_FAST_SAMPLE_MASK = 0x1;
+const int ETAG_BITS_MASK = 0x7;
+
 void loop()
 {
-    // read ecg data from max30003 fifo
-    uint32_t ecg_fifo = ecg.max30003_read_register(max30003::ECG_FIFO);
+    // // read ecg data from max30003 fifo
+    // uint32_t ecg_fifo = ecg.max30003_read_register(max30003::ECG_FIFO);
+    //
+    // // shift out ETG[5:3] & PTG[2:0] bits & extract 18 bits data as signed
+    // // integer for serial plotting
+    // int16_t ecg_sample = ecg_fifo >> 6;
+    //
+    // // print ecg voltage to display in serial plotter
+    // // Serial.println(ecg_sample);
+    //
+    // // read r-to-r data from max30003
+    // uint32_t r_to_r = ecg.max30003_read_register(max30003::RTOR);
+    //
+    // // extract 14 bits data from r_to_r register
+    // r_to_r = ((r_to_r >> 10) & 0x3fff);
+    // // Serial.print(r_to_r * 8);
+    // // Serial.print(",");
+    //
+    // // calculate BPM
+    // float bpm = 1.0f / (r_to_r * RTOR_LSB_RES / 60.0f);
+    // // Serial.println(bpm);
+    //
+    // // r_to_r must be multiplied by 8 to get the time interval in millisecond
+    // // 8ms resolution is for 32768Hz master clock
+    // send_data_to_pde_plot(ecg_sample, (uint16_t)r_to_r * 8, (int16_t)bpm);
+    //
+    // delay(8);
 
-    // shift out ETG[5:3] & PTG[2:0] bits & extract 18 bits data as signed
-    // integer for serial plotting
-    int16_t ecg_sample = ecg_fifo >> 6;
+    if (ecg_int_flag)
+    {
+        ecg_int_flag = 0;
+        uint32_t status = ecg.max30003_read_register(max30003::STATUS);
 
-    // print ecg voltage to display in serial plotter
-    // Serial.println(ecg_sample);
+        uint32_t ecg_fifo, sample_count, etag_bits[32];
+        int16_t ecg_sample[32];
 
-    // read r-to-r data from max30003
-    uint32_t r_to_r = ecg.max30003_read_register(max30003::RTOR);
+        if ((status & EINT_STATUS_MASK) == EINT_STATUS_MASK)
+        {
+            // reset sample counter
+            sample_count = 0;
 
-    // extract 14 bits data from r_to_r register
-    r_to_r = ((r_to_r >> 10) & 0x3fff);
-    // Serial.print(r_to_r * 8);
-    // Serial.print(",");
+            do
+            {
+                // read ecg data from max30003 fifo
+                ecg_fifo = ecg.max30003_read_register(max30003::ECG_FIFO);
 
-    // calculate BPM
-    float bpm = 1.0f / (r_to_r * RTOR_LSB_RES / 60.0f);
-    // Serial.println(bpm);
+                // shift out ETAG[5:3] & PTAG[2:0] bits & extract 18 bits data
+                // as signed integer for serial plotting
+                ecg_sample[sample_count] = ecg_fifo >> 8;
 
-    // r_to_r must be multiplied by 8 to get the time interval in millisecond
-    // 8ms resolution is for 32768Hz master clock
-    send_data_to_pde_plot(ecg_sample, (uint16_t)r_to_r * 8, (int16_t)bpm);
+                // extract ETAG[5:3] bits
+                etag_bits[sample_count] = (ecg_fifo >> 3) & ETAG_BITS_MASK;
 
-    delay(8);
+                // increase sample count
+                sample_count++;
+
+                // if sample was last in FIFO, then exit
+            } while (etag_bits[sample_count - 1] == FIFO_VALID_SAMPLE_MASK ||
+                     etag_bits[sample_count - 1] == FIFO_FAST_SAMPLE_MASK);
+
+            // if FIFO is overflowed, reset FIFO
+            if (etag_bits[sample_count - 1] == FIFO_OVF_MASK)
+                ecg.max30003_write_register(max30003::FIFO_RST, 0);
+
+            // Print results
+            for (int i = 0; (sample_count > 1 && i < sample_count); i++)
+            {
+                Serial.println(ecg_sample[i]);
+            }
+        }
+    }
 }
